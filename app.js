5888155231964bfa9a749727e98e85a36628468c
asadnaved9 <asadnaved4@gmail.com>
2025-12-08 19:21:35 +0530

    Likes and Unlikes section with Logic Done

--- app.js
+++ app.js
@@ -27,6 +27,19 @@ app.get('/profile', isLoggedIn, async (req, res) =>{
     res.render("profile", {user});
 })
 
+app.get('/like/:id', isLoggedIn, async (req, res) =>{ 
+    let post = await postModel.findOne({_id: req.params.id}).populate("user");
+    if(post.likes.indexOf(req.user.userid) === -1){
+        post.likes.push(req.user.userid);
+    }
+    else{
+        post.likes.splice(post.likes.indexOf(req.user.userid), 1);
+    }
+    await post.save();
+    res.redirect("/profile");
+    
+})
+
 app.post('/post', isLoggedIn, async (req, res) =>{// isLoggedIn means post tb hi hoga jb app logged in ho.
     let user = await userModel.findOne({email: req.user.email});
     let {content} = req.body;
